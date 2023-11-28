f296776b7640b60744e3b879d1d787ae58840c07
notV4l <imV4l@proton.me>
2023-11-28 16:30:30 +0100

    default visible

--- app/components/ScrollButton.tsx
+++ app/components/ScrollButton.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ChevronDown } from "../icons/ChevronDown";
-import { useIsMobile } from "../hooks/useIsMobile";
 
 const scrollTo = (id: string) => {
   const el = document.getElementById(id);
@@ -11,11 +10,8 @@ const scrollTo = (id: string) => {
 };
 
 export const ScrollButton = () => {
-  const {isMobile} = useIsMobile();
-
-  if (isMobile) return null;
   return (
-    <div className="p-8 bg-dojo-blue-800">
+    <div className="p-8 bg-dojo-blue-800 hidden md:block">
       <button
         onClick={() => scrollTo("code")}
         className="rounded-full border p-2 border-dojo-blue-300 hover:bg-dojo-blue-300 hover:border-dojo-blue-300 hover:text-dojo-blue-800 bg-dojo-blue-800 z-10"
