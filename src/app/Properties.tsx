e2cfba39ec0b6c2aff5f825e67dbdb567c1f2d63
Jasper Dansercoer <jasper@codictive.be>
2024-04-12 11:09:58 +0200

    Added animation on the properties

--- src/app/Properties.tsx
+++ src/app/Properties.tsx
@@ -3,6 +3,7 @@
 import { Container } from "@/components/Container";
 import { Box, Flex, Grid, Text } from "@chakra-ui/react";
 import React, { useLayoutEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 const allProperties = [
   {
@@ -151,11 +152,40 @@ export function Properties() {
               bg="url('/images/tile-bg.png')"
               bgSize={gridSize}
               bgPos={property.bgPos}
+              position="relative"
+              cursor="pointer"
             >
-              {property.icon}
+              <Box zIndex={5}>{property.icon}</Box>
               <Text textStyle="title3" alignSelf="flex-start">
                 {property.name}
               </Text>
+              <motion.div
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  bottom: 0,
+                  borderRadius: "16px",
+                  background: "url('/images/tile-bg.png')",
+                  backgroundSize: gridSize,
+                  backgroundPosition: property.bgPos,
+                  maskImage:
+                    "radial-gradient(ellipse 0% 0% at 50% 50%, black 100%, transparent 50%)",
+                  display: "flex",
+                  alignItems: "flex-end",
+                  padding: "24px",
+                }}
+                whileHover={{
+                  maskImage:
+                    "radial-gradient(ellipse 70% 70% at 50% 50%, black 100%, transparent 50%)",
+                }}
+              >
+                <Text textStyle="bodyText">
+                  Here is a medium length paragraph about this feature and maybe
+                  an extra line.
+                </Text>
+              </motion.div>
             </Flex>
           );
         })}
