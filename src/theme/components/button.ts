72549ced6b942ff11bb79ae615e43faa6c47a08d
RareSecond <jasper@codictive.be>
2024-04-19 15:34:10 +0200

    Added hover effect to buttons

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -1,10 +1,47 @@
 export const Button = {
   baseStyle: {
-    bg: "button.purple",
     py: 3,
     px: 8,
     borderRadius: "full",
     textStyle: "buttonText",
+    transition: "all 0.4s",
+    zIndex: 1,
+    overflow: "hidden",
+    _before: {
+      content: "''",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      borderRadius: "full",
+      bg: "button.purple",
+      zIndex: -2,
+    },
+    _after: {
+      content: "''",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      bottom: 0,
+      width: "0%",
+      borderRadius: "full",
+      bg: "badge.yellow",
+      transition: "all 0.4s",
+      zIndex: -1,
+    },
+    _hover: {
+      color: "text.black",
+
+      _after: {
+        width: "100%",
+      },
+
+      path: {
+        fill: "text.black",
+        transition: "all 0.4s",
+      },
+    },
   },
   variants: {
     showArrow: {
