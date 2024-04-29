e59ffd79edb7f6da6b22e3082c015a7d9743a417
RareSecond <jasper@codictive.be>
2024-04-29 12:19:34 +0200

    Updated single game display

--- src/app/games/SingleGame.tsx
+++ src/app/games/SingleGame.tsx
@@ -7,13 +7,19 @@ export function SingleGame({
 }: {
   game: {
     name: string;
-    logo: JSX.Element;
+    image: string;
   };
 }) {
   return (
-    <div className="flex flex-col items-end justify-between rounded-[16px] border-[1px] border-[#4E38E1] p-[24px] hover:bg-[#4E38E1]">
-      {game.logo}
-      <Text textStyle="title3" className="self-start">
+    <div
+      className="group relative aspect-square cursor-pointer flex-col items-end justify-between rounded-[16px] border-[8px] border-[#4E38E1] bg-cover bg-center p-[24px]"
+      style={{ backgroundImage: `url('${game.image}')` }}
+    >
+      <div className="absolute inset-0 top-full flex items-end bg-gradient-to-t from-[#4A35DA] transition-all group-hover:top-0" />
+      <Text
+        textStyle="title3"
+        className=" absolute bottom-4 left-4 translate-y-full opacity-0 transition-all delay-0 group-hover:translate-y-0 group-hover:opacity-100 group-hover:delay-150"
+      >
         {game.name}
       </Text>
     </div>
