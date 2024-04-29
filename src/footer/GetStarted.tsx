4d4e7c30e14b7950104165e4508636afcbc31368
RareSecond <jasper@codictive.be>
2024-04-29 12:37:01 +0200

    Updated design of CTA button

--- src/footer/GetStarted.tsx
+++ src/footer/GetStarted.tsx
@@ -8,19 +8,22 @@ import * as Tooltip from "@radix-ui/react-tooltip";
 export function GetStarted() {
   const [isCopied, setCopied] = useClipboard(
     "curl -L https://dojoengine.org | bash",
+    { successDuration: 2000 },
   );
 
   return (
     <div className="from-36% flex items-center justify-center gap-12 bg-gradient-to-t from-[#05163100] to-[#061732] p-40">
       <Text textStyle="title1"> Get started with our CLI:</Text>
       <Tooltip.Provider delayDuration={0}>
-        <Tooltip.Root>
+        <Tooltip.Root open={isCopied || undefined}>
           <Tooltip.Trigger>
-            <div onClick={setCopied} className="group relative p-[2px]">
-              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-[#6366f1] to-[#a855f7]"></div>
-              <div className="relative rounded-full bg-badge-red px-8 py-2 font-['IBM_Plex_Mono_SemiBold'] text-text-black transition-all duration-200 hover:bg-[transparent] hover:text-text-white">
-                curl -L https://dojoengine.org | bash
-              </div>
+            <div
+              onClick={setCopied}
+              className="group relative z-0 overflow-hidden rounded-full px-10 py-5 font-['IBM_Plex_Mono_SemiBold'] text-text-black"
+            >
+              <div className="absolute inset-0 z-[-2] bg-badge-red" />
+              <div className="absolute bottom-0 left-0 top-0 z-[-1] w-0 bg-badge-yellow transition-all duration-[400ms] group-hover:w-full" />
+              curl -L https://dojoengine.org | bash
             </div>
           </Tooltip.Trigger>
           <Tooltip.Content>
