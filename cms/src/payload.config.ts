e3ca3c658b3cd8455d98364db2b864be0e38adf4
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-07-25 15:49:31 +1000

    config s3

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -6,6 +6,9 @@ import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
 
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
+
 import Users from "./collections/Users";
 import Posts from "./collections/Posts";
 import Events from "./collections/Events";
@@ -48,7 +51,27 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
-  plugins: [payloadCloud()],
+  plugins: [
+    payloadCloud(),
+    cloudStorage({
+      collections: {
+        // Enable cloud storage for Media collection
+        media: {
+          // Create the S3 adapter
+          adapter: s3Adapter({
+            config: {
+              credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+              },
+              region: process.env.S3_REGION,
+            },
+            bucket: process.env.S3_BUCKET,
+          }),
+        },
+      },
+    }),
+  ],
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
